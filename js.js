7e1a938ff67f83593593c4faaf82ecf264f565dc
pandavandama <strangemikados@gmail.com>
2020-04-15 23:55:40 +0300

    update_dropdown

--- js.js
+++ js.js
@@ -1,17 +1,18 @@
 
 let i=0;
-let check = 0;
+
 let _buket = document.getElementById('buket');
 let anim = requestAnimationFrame(move);
 
 function move(){
-    if(check%2==0)
+    
     i+= 0.1;
-    else i--;
+   
 
     _buket.style.transform = `rotate(${i}deg)`;
 
     requestAnimationFrame(move);
 }
 
-move();
+
+
