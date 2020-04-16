01c5b9c1962bdaf7cf93fddab71dea0ffd53c96d
pandavandama <strangemikados@gmail.com>
2020-04-16 20:31:42 +0300

    normalno teperi a ne musorka

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ let _buket = document.getElementById('buket');
 let anim = requestAnimationFrame(move);
 
 function move(){
-    
+    console.log('jopa')
     i+= 0.1;
    
 
---
+++ js/text_for_elems.js
@@ -0,0 +1,42 @@
+
+
+let help_symbol = '';
+
+if(location.href.indexOf('index')<0) help_symbol ='.';
+
+let elem_obj = [
+    {   
+        img_src:help_symbol+'./image/roses/1.jpg',
+        text:'dsadassad'
+        
+    },
+    {
+        img_src:help_symbol+'./image/roses/2.jpg',
+        text:'fsasfasfsafsa'
+    },
+    {
+        img_src:help_symbol+'./image/roses/3.jpg',
+        text:'fsasfasfsafsa'
+    },
+    {
+        img_src:help_symbol+'./image/roses/4.jpg',
+        text:'fsasfasfsafsa'
+    },
+    {
+        img_src:help_symbol+'./image/roses/5.jpg',
+        text:'fsasfasfsafsa'
+    },
+    {
+        img_src:help_symbol+'./image/roses/6.jpg',
+        text:'fsasfasfsafsa'
+    },
+    {
+        img_src:help_symbol+'./image/roses/7.jpg',
+        text:'fsasfasfsafsa'
+    },
+    {
+        img_src:help_symbol+'./image/aloe/1.jpg',
+        text:'fsasfasfsafsa'
+    }
+];
+
